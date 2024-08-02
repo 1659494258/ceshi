c29eb77bb164830adcc284e96cbade3e149abac4
xiebiao <1659494258@qq.com>
2024-08-02 10:12:36 +0800

    asdas

--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     proxy: {
       '/config': {
         // target: 'http://10.2.65.205:9202/',
-        target: 'http://192.168.66.16:9200/',
+        target: 'http://192.168.66.16:9200/12312',
         // target: 'http://192.168.66.122:9202/',
         changeOrigin: true,
         secure: false, // 请求是否为https
