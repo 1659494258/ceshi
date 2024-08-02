8f25d0877b99241c74a2c7b07e86c0025a64bc55
xiebiao <1659494258@qq.com>
2024-08-02 09:38:36 +0800

    back

--- src/views/home/back.vue
+++ src/views/home/back.vue
@@ -1,39 +1,27 @@
 <template>
-  <div class="">
-    dsfksd
-  </div>
+  <div class="">dsfksddfagafsdg</div>
 </template>
-  <script setup lang="ts">
-  import { ref ,onMounted,inject  } from 'vue'
-  import { useRoute, useRouter } from 'vue-router';
-  const router = useRouter();
+<script setup lang="ts">
+import { ref, onMounted, inject } from "vue";
+import { useRoute, useRouter } from "vue-router";
+const router = useRouter();
 // 跳转至指定页面
 
-  const reload = inject('reload')
-  
-  const url = ref()
-  onMounted(() => {
-    router.replace({
-        path:"/home/index",
-    })
-    console.log('router:', router.currentRoute.value.query.path)
-    url.value = router.currentRoute.value.query.path
-    const param =  {
-        UserID: 'admin',
-        Password: '000000als',
-    }
-      console.log(param)
-    // localStorage.setItem('ACCESS_TOKEN', res.data);
-    // router.push("/flow")
-    // router.push({
-    //     path: '/index',
-    //     query: {
-    //         ...url
-    //     }
-    // })
-    })
+const reload = inject("reload");
 
-  </script>
-  
-<style scoped>
-</style>
+const url = ref();
+onMounted(() => {
+  router.replace({
+    path: "/home/index",
+  });
+  console.log("router:", router.currentRoute.value.query.path);
+  url.value = router.currentRoute.value.query.path;
+  const param = {
+    UserID: "admin",
+    Password: "000000als",
+  };
+  console.log(param);
+});
+</script>
+
+<style scoped></style>
